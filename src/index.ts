642f8f69609debccc8dbaee1b5d20d1f9940d93a
Gustavo Emanuel Farias Rosa <gustavo.emanuel01@outlook.com>
2021-12-11 12:58:23 -0300

    Documentação com swagger jsdocs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
 import express from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerDocument from "./swagger.json";
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Teste API",
+            version: "1.0.0",
+        },
+    },
+    apis: ["./src/routes/*.ts"], // files containing annotations as above
+};
+
+const openapiSpecification = swaggerJsdoc(options);
 
 const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.use(express.json());
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -7,20 +7,123 @@ import { turnUserAdminController } from "../modules/users/useCases/turnUserAdmin
 
 const usersRoutes = Router();
 
+/**
+ * @openapi
+ * /users:
+ *   post:
+ *     description: Post User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The user's name.
+ *                 example: Leanne Graham
+ *               email:
+ *                 type: string
+ *                 description: Description.
+ *                 example: teste@teste.com
+ *     responses:
+ *       201:
+ *         description: Return the user.
+ */
 usersRoutes.post("/", (request, response) =>
   createUserController.handle(request, response)
 );
 
+/**
+ * @openapi
+ * /users/{user_id}/admin:
+ *   patch:
+ *     description: Turn user admin
+ *     parameters:
+ *       - in: path
+ *         name: user_id
+ *         required: true
+ *         description: ID from user.
+ *         schema:
+ *           type: string
+ *
+ *     responses:
+ *       200:
+ *         description: Turn user a Admin.
+
+ */
 usersRoutes.patch("/:user_id/admin", (request, response) =>
   turnUserAdminController.handle(request, response)
 );
 
+/**
+ * @openapi
+ * /users/{user_id}:
+ *   get:
+ *     description: Get user
+ *     parameters:
+ *       - in: path
+ *         name: user_id
+ *         required: true
+ *         description: ID from user.
+ *         schema:
+ *           type: string
+ *
+ *     responses:
+ *       200:
+ *         description: Return the user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               properties:
+ *                 name:
+ *                   type: string
+ */
 usersRoutes.get("/:user_id", (request, response) =>
   showUserProfileController.handle(request, response)
 );
 
+/**
+ * @openapi
+ * /users:
+ *   get:
+ *     description: Get users
+ *     parameters:
+ *       - in: header
+ *         name: user_id
+ *         required: true
+ *         description: ID from user.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Return the user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               properties:
+ *                 name:
+ *                   type: string
+ */
 usersRoutes.get("/", (request, response) =>
   listAllUsersController.handle(request, response)
 );
 
 export { usersRoutes };
+
+/**
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The user's name.
+ *                 example: Leanne Graham
+ */
